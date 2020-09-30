30b6da4f1dd56154ebb124b6b9060d209673bb35
Dave Borowski <dmborowski@msn.com>
2020-09-29 21:40:48 -0600

    created an init and update function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,70 +1,67 @@
-// // Use D3 to select the dropdown menu
-// var dropdownMenu = d3.select("#selDataset");
-// // Assign the value of the dropdown menu option to a variable
-// // var dataset = dropdownMenu.node().value;
-// var dataset = dropdownMenu.selectAll('names').data(samples);
 function plots(id) {
-
+    console.log(id);
     d3.json("samples.json").then(sampleData => {
         console.log(sampleData)
+        var data = sampleData.samples[0];
+        console.log(data);           
 
-        var ids = sampleData.samples[0].otu_ids;
-        console.log(ids)
+            var ids = data.otu_ids;
+            console.log(ids)
 
-        var values = sampleData.samples[0].sample_values;
-        console.log(values)
+            var values = sampleData.samples[1].sample_values;
+            console.log(values)
 
-        var labels = sampleData.samples[0].otu_labels;
-        console.log(labels)
+            var labels = sampleData.samples[1].otu_labels;
+            console.log(labels)
 
-        var otuTop10 = sampleData.samples[0].sample_values.slice(0,10).reverse();
-        console.log(otuTop10)
+            var otuTop10 = sampleData.samples[1].sample_values.slice(0,10).reverse();
+            console.log(otuTop10)
 
-        var idTop10 = (sampleData.samples[0].otu_ids.slice(0,10)).map(d => "OTU " + d);
-        console.log(idTop10)
+            var idTop10 = (sampleData.samples[1].otu_ids.slice(0,10)).map(d => "OTU " + d);
+            console.log(idTop10)
 
-        var nameTop10 = sampleData.samples[0].otu_labels.slice(0,10);
-        console.log(nameTop10)
+            var nameTop10 = sampleData.samples[1].otu_labels.slice(0,10);
+            console.log(nameTop10)
 
-            var trace1 = {
-                x: otuTop10,
-                y: idTop10,
-                text: nameTop10,
-                type: "bar",
-                orientation: "h"
-            };
+                var trace1 = {
+                    x: otuTop10,
+                    y: idTop10,
+                    text: nameTop10,
+                    type: "bar",
+                    orientation: "h"
+                };
 
-            var data = [trace1];
+                var data = [trace1];
 
-            var layout = {
-                title: " Top 10 OTU per subject",
-                xaxis: { title: "Number of Microbes"},
-                yaxis: { title: "OTU"}                
-            };
+                var layout = {
+                    title: " Top 10 OTU per subject",
+                    xaxis: { title: "Number of Microbes"},
+                    yaxis: { title: "OTU"}                
+                };
 
-            Plotly.newPlot("bar", data, layout);
+                Plotly.newPlot("bar", data, layout);
 
-            var trace2 = {
-                x: ids,
-                y: values,
-                mode: 'markers',
-                marker: {
-                    color: ids,
-                    size: values
-                },
-                text: labels
-            };
+                var trace2 = {
+                    x: ids,
+                    y: values,
+                    mode: 'markers',
+                    marker: {
+                        color: ids,
+                        size: values
+                    },
+                    text: labels
+                };
 
-            var data2 = [trace2];
+                var data2 = [trace2];
 
-            var layout2 = {
-                title: "Type and amount of OTU's",
-                xaxis: { title: "OTU ID"},
-                yaxis: { title: "Number of Microbes"}
-            };
+                var layout2 = {
+                    title: "Type and amount of OTU's",
+                    xaxis: { title: "OTU ID"},
+                    yaxis: { title: "Number of Microbes"}
+                };
 
-            Plotly.newPlot("bubble", data2, layout2);
-
+                Plotly.newPlot("bubble", data2, layout2);
+        // }
     });
 };
 
@@ -80,16 +77,17 @@ var select = d3.select('#selDataset');
         plots(data.names[0]);
 
     d3.select('#selDataset').on('change', updatePlotly);
-
-    function updatePlotly() {
-        // Use D3 to select the dropdown menu
-        var dropdownMenu = d3.select("#selDataset");
-        // Assign the value of the dropdown menu option to a variable
-        var id = dropdownMenu.property("value");
-
-        plots(id);
-    };
     });
+};
+
+function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var id = dropdownMenu.property("value");
+
+    plots(id);
+    console.log(id);
 };
 
 init();
