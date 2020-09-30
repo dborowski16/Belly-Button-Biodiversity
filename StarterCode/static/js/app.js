b54ea4db65794ee70d78270c205e04c1d7c1141d
Dave Borowski <dmborowski@msn.com>
2020-09-30 16:47:35 -0600

    updated color shading on wash gauge

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -106,7 +106,16 @@ function plots(id) {
                           gauge: {
                             axis: { range: [null, 10] },
                             steps: [
-                              { range: [0, 1], color: "rgb" }
+                              { range: [0, 1], color: "rgba(0,105,11,.5)"},
+                              { range: [1, 2], color: "rgba(10,120,22,.5)"},
+                              { range: [2, 3], color: "rgba(14,127,0,.5)"},
+                              { range: [3, 4], color: "rgba(110,154,22,.5)"},
+                              { range: [4, 5], color: "rgba(170,202,42,.5)"},
+                              { range: [5, 6], color: "rgba(202,209,95,.5)"},
+                              { range: [6, 7], color: "rgba(210,206,145,.5)"},
+                              { range: [7, 8], color: "rgba(232,226,202,.5)"},
+                              { range: [8, 9], color: "rgba(240, 230,215,.5)"},
+                              { range: [9, 10], color: "rgba(255,255,255,0)"}
                             ],
                           }
                         }
